0da42475056d61b388b751ab5031c7ae1554f42d
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-05-23 18:48:54 +0200

    Cambio de nombre por el problema de altributo

--- tfm/ObtencionDatos/main_collab.py
+++ tfm/ObtencionDatos/main_collab.py
@@ -5,11 +5,6 @@ import os, datetime
 import time, logging, yaml
 from pathlib import Path
 
-# Obtener la ruta del script actual
-ruta_script = os.path.abspath("__file__")
-os.environ['PROJECT_ROOT'] = r'C:\PROYECTOS\PyCharm\pythonrun\recuperacion_informacion_modelos_lenguaje\tfm'
-
-
 # Abrir y leer el archivo YAML
 with open(Path(os.getenv('PROJECT_ROOT')) / 'config/config.yml', 'r') as file:
     config = yaml.safe_load(file)
@@ -39,30 +34,9 @@ logging.basicConfig(filename=PATH_BASE / config['logs_config']['ruta_salida_logs
 # Creamos el logger
 logger = logging.getLogger()
 
-BOE = DescargaBOE()
-i = 0
-while True:
-    BOE.establecer_offset(i)
-    if(BOE.generar_dataset() > 200):
-        break
-    time.sleep(1)
-    i += 1
-BOE.obtener_dataset_final()
-
-df = BOE.obtener_dataset_final()
-folder_paquete = config['scrapping']['ruta']
-folder_data = config['scrapping']['descarga_datos']
-df.to_csv(f'{directorio_proyecto}/{folder_paquete}/datos/csv_boes_oferta_publica.csv', sep='|')
 
 BOCyL = DescargaBOCyL()
-i = 0
-while True:
-    BOCyL.establecer_offset(i)
-    if(BOCyL.generar_dataset() > 20):
-        break
-    time.sleep(1)
-    i += 1
-BOCyL.obtener_dataset_final()
+BOCyL.initialize_download()
 
-df_BOCyL = BOCyL.obtener_dataset_final()
-df.to_csv(f'{directorio_proyecto}/{folder_paquete}/datos/csv_bocyls_oferta_publica.csv', sep='|')
+BOE = DescargaBOE()
+BOE.initialize_download()

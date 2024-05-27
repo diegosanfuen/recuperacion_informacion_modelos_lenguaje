8dfa8f9019b31bc71de23cb86eaa2a1679c1f345
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-05-27 14:49:29 +0200

    ChatBot

--- tfm/ObtencionDatos/__init__.py
+++ tfm/ObtencionDatos/__init__.py
@@ -2,5 +2,5 @@
 nombre_paquete = "ObtenerDatos"
 
 # Importamos los módulos que queremos que estén disponibles
-from DescargaBOE import DescargaBOE
-from DescargaBOCyL import DescargaBOCyL
+from ObtenerDatos.DescargaBOE import DescargaBOE
+from ObtenerDatos.DescargaBOCyL import DescargaBOCyL

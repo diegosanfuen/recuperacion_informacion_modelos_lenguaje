0675444104469a2717e41e25a8c84af19ce0b48c
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-05-29 20:41:50 +0200

    ChatBot

--- scripts/instalar_requirements.sh
+++ scripts/instalar_requirements.sh
@@ -2,15 +2,10 @@
 pip install langchain
 pip install langchain-core
 pip install langchain-community
+pip install langchain_text_splitters
 pip install faiss-gpu
 pip install python-dotenv
 pip install gradio
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from langchain import hub
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OllamaEmbeddings
 pip install langchainhub
 pip install pdfplumber
 pip install sumy
